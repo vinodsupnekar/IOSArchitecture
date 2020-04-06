02ba0a22c776ab116b0de3732e2275a303beeb18
Vinod Supnekar <vinod.supnekar@playerzpot.com>
2020-04-06 23:29:31 +0530

    Initial Commit

---
+++ CleanIOSArchitecture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CleanIOSArchitecture
+//
+//  Created by PlayerzPotMedia on 06/04/20.
+//  Copyright © 2020 PlayerzPotMedia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
